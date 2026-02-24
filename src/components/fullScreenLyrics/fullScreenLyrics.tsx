a13ec0498a1dc6224fd8c6aa248f10f1845500ef
Lucas <lucas@clinicsense.com>
2026-02-24 18:15:36 -0500

    remove spacing in fullscreen

--- src/components/fullScreenLyrics/fullScreenLyrics.tsx
+++ src/components/fullScreenLyrics/fullScreenLyrics.tsx
@@ -16,17 +16,17 @@ export const FullScreenLyrics = () => {
                         content={fullScreenContent || ""}
                         hideSettings
                         editable
-                        bottomMargin={80}
+                        bottomMargin={0}
                     />
                     <button
-                        className='z-20 top-[50px] right-[50px] absolute cursor-pointer p-[10px] bg-white-500 rounded-full hover:bg-blue-500 hover:text-white'
+                        className='z-20 bottom-[5px] right-[5px] absolute cursor-pointer p-[10px] bg-white-500 rounded-full hover:bg-blue-500 hover:text-white'
                         onClick={() => setShowFullScreen(undefined)}
                     >
                         <HiArrowsPointingIn className="size-[18px]" />
                     </button>
                 </div>
             }
-            className="w-[500px] h-9/10 border-0"
+            className="w-screen h-screen"
             bodyStyling='px-[15px]'
             backDropStyling="bg-white/100"
         />
